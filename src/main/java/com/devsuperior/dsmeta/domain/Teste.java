f17bc2b28a9f0058962cfcefff0971abea98eb60
GabrielTavares96 <gabriel_sbo1996@hotmail.com>
2022-07-14 09:04:28 -0300

    add teste

---
+++ src/main/java/com/devsuperior/dsmeta/domain/Teste.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsmeta.domain;
+
+public class Teste {
+
+    private String nome;
+    private String rua;
+    private String bairro;
+    private String complemento;
+    private String cidade;
+    private String estado;
+
+}
